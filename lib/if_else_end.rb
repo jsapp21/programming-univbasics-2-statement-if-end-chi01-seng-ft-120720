af887d543fb62a9e4c4710daaad4a2887592fdcb
James Sapp <jameseisapp@gmail.com>
2020-10-30 21:06:46 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,7 @@
-current_time = Time.now 
-current_time = current_time.to_i 
-
+current_time = Time.now
+current_time = current_time.to_i
+ 
 if current_time.even?
-  puts "Evan!"
+  puts "Even!"
 else
   puts "Odd!"
-end
