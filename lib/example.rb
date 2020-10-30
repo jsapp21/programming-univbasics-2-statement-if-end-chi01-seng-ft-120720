1001d12f59cd417972d05ec2144e691835989bb2
James Sapp <jameseisapp@gmail.com>
2020-10-30 20:27:19 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++
@@ -1,8 +0,0 @@
-chance_of_rain = 0.2 
-if chance_of_rain <= 0.25
-  puts "pack a sun shelter!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "pack an umbrella!"
-else
-  puts "stay at home!"
-end
